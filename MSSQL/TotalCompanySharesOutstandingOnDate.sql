c786e4020741bccd0c2de0e444b79c5767118e70
Avinash1125 <63236641+Avinash1125@users.noreply.github.com>
2020-07-29 23:08:03 +0530

    updated

--- MSSQL/TotalCompanySharesOutstandingOnDate.sql
+++ MSSQL/TotalCompanySharesOutstandingOnDate.sql
@@ -1,18 +1,14 @@
-/***********************************************************************
-Total Company Shares Outstanding on a specific date
+/***********************************************************************************************
+Total Company Shares Outstanding On A Specific Date
 
 Packages Required:
-Finl Capital Structure Equity
-Base Equity Security
-Base Security
+Finl Capital Structure Equity, Base Equity Security, Base Security
 
 Universal Identifiers:
 companyId
 
 Primary Columns Used:
-componentId
-securityId
-tradingItemId
+componentId, securityId, tradingItemId
 
 Database_Type:
 MSSQL
@@ -21,16 +17,16 @@ Query_Version:
 V1
 
 Query_Added_Date:
-25\05\2020
+25/05/2020
 
 DatasetKey:
 10, 17
 
-Calculate company-level, aggregated shares outstanding on a particular 
-historical date, incorporating both traded and non-traded shares 
-outstanding and any relevant conversion factors.
+Calculate company-level, aggregated shares outstanding on a particular  historical date, incorporating both traded and non-traded shares  outstanding and any relevant conversion factors
 
-***********************************************************************/  
+***********************************************************************************************/
+
+ 
 
  DECLARE @CoID INT  
  DECLARE @sharesDate DATE  
@@ -40,58 +36,58 @@ outstanding and any relevant conversion factors.
  
  SELECT so.companyId, SUM(so.ConvertedTCSO) AS TCSO  
  FROM   (   
-		 -- Traded Securities:    
-		SELECT      ec.companyId     
-		, ec.componentId     
-		, ec.securityId     
-		, ti.tradingItemId     
-		, ti.tickerSymbol     
-		, ec.capitalStructureDescription AS 'SecurityName'     
-		, ec.TCSOFlag     
-		, (CASE WHEN ec.primaryClassForTCSO = 2 THEN '1' ELSE '0' END) AS 'PrimaryFlag'    
-		, '0' AS 'NonTradedFlag'     
-		, ec.classConversionFactorForTCSO AS 'TSCOConvFactor'     
-		, so.sharesOutstanding     
-		, so.sharesOutstanding * ec.classConversionFactorForTCSO AS 'ConvertedTCSO'  
-		  
-		FROM ciqCapStEqComponent ec (NOLOCK)    
-		JOIN ciqTradingItem ti (NOLOCK)     ON ti.securityId = ec.securityId     
-		AND ti.primaryFlag = 1    
-		JOIN ciqPriceEquitySharesOutstdg so (NOLOCK)     ON so.tradingItemId = ti.tradingItemId     
-		AND @sharesDate BETWEEN so.fromDate AND ISNULL(so.toDate,'2076-6-6')    
-		JOIN ciqSecurity sec (NOLOCK)     ON ti.securityId = sec.securityId 
-		  
-		WHERE 1=1    
-		AND ec.TCSOFlag = 2 -- equity component should be included in total shares outstanding (as of today only, but we assume this flag usually does not change historically)    
-		AND @sharesDate BETWEEN ISNULL(sec.securityStartDate,'1/1/1900') AND ISNULL(sec.securityenddate,'2076-6-6')    
-		AND ec.companyId = @CoID -- Comment this out if you want full universe    
-		  
-		UNION       
-		-- Non Traded Securities:    
-		SELECT ec.companyId     
-		, ec.componentId     
-		, ec.securityId     
-		, NULL AS tradingItemId     
-		, NULL AS tickerSymbol     
-		, ec.capitalStructureDescription AS 'SecurityName'     
-		, ec.TCSOFlag     
-		, (CASE WHEN ec.primaryClassForTCSO = 2 THEN '1' ELSE '0' END) AS 'PrimaryFlag'     
-		, '1' AS 'NonTradedFlag'          
-		, ec.classConversionFactorForTCSO AS 'TSCOConvFactor'     
-		, ntso.sharesOutstanding     
-		, ntso.sharesOutstanding * ec.classConversionFactorForTCSO AS 'ConvertedTCSO'    
+                 -- Traded Securities:    
+                SELECT      ec.companyId     
+                , ec.componentId     
+                , ec.securityId     
+                , ti.tradingItemId     
+                , ti.tickerSymbol     
+                , ec.capitalStructureDescription AS 'SecurityName'     
+                , ec.TCSOFlag     
+                , (CASE WHEN ec.primaryClassForTCSO = 2 THEN '1' ELSE '0' END) AS 'PrimaryFlag'    
+                , '0' AS 'NonTradedFlag'     
+                , ec.classConversionFactorForTCSO AS 'TSCOConvFactor'     
+                , so.sharesOutstanding     
+                , so.sharesOutstanding * ec.classConversionFactorForTCSO AS 'ConvertedTCSO'  
+                  
+                FROM ciqCapStEqComponent ec (NOLOCK)    
+                JOIN ciqTradingItem ti (NOLOCK)     ON ti.securityId = ec.securityId     
+                AND ti.primaryFlag = 1    
+                JOIN ciqPriceEquitySharesOutstdg so (NOLOCK)     ON so.tradingItemId = ti.tradingItemId     
+                AND @sharesDate BETWEEN so.fromDate AND ISNULL(so.toDate,'2076-6-6')    
+                JOIN ciqSecurity sec (NOLOCK)     ON ti.securityId = sec.securityId 
+                  
+                WHERE 1=1    
+                AND ec.TCSOFlag = 2 -- equity component should be included in total shares outstanding (as of today only, but we assume this flag usually does not change historically)    
+                AND @sharesDate BETWEEN ISNULL(sec.securityStartDate,'1/1/1900') AND ISNULL(sec.securityenddate,'2076-6-6')    
+                AND ec.companyId = @CoID -- Comment this out if you want full universe    
+                  
+                UNION       
+                -- Non Traded Securities:    
+                SELECT ec.companyId     
+                , ec.componentId     
+                , ec.securityId     
+                , NULL AS tradingItemId     
+                , NULL AS tickerSymbol     
+                , ec.capitalStructureDescription AS 'SecurityName'     
+                , ec.TCSOFlag     
+                , (CASE WHEN ec.primaryClassForTCSO = 2 THEN '1' ELSE '0' END) AS 'PrimaryFlag'     
+                , '1' AS 'NonTradedFlag'          
+                , ec.classConversionFactorForTCSO AS 'TSCOConvFactor'     
+                , ntso.sharesOutstanding     
+                , ntso.sharesOutstanding * ec.classConversionFactorForTCSO AS 'ConvertedTCSO'    
 
-		FROM ciqCapStEqComponent ec (NOLOCK)    
-		JOIN ciqNonTradedSharesOutstding ntso (NOLOCK) ON ntso.componentId = ec.componentId     
-		AND @sharesDate BETWEEN ntso.fromDate AND ISNULL(ntso.toDate,'2076-6-6')    
-		JOIN ciqSecurity sec (NOLOCK)    ON EC.securityId = sec.securityId    
+                FROM ciqCapStEqComponent ec (NOLOCK)    
+                JOIN ciqNonTradedSharesOutstding ntso (NOLOCK) ON ntso.componentId = ec.componentId     
+                AND @sharesDate BETWEEN ntso.fromDate AND ISNULL(ntso.toDate,'2076-6-6')    
+                JOIN ciqSecurity sec (NOLOCK)    ON EC.securityId = sec.securityId    
 
-		WHERE 1=1    
-		AND ec.TCSOFlag = 2 -- equity component should be included in total shares outstanding (as of today only, but we assume this flag usually does not change historically)    
-		AND @sharesDate BETWEEN ISNULL(sec.securityStartDate,'1/1/1900') 
-		AND ISNULL(sec.securityenddate,'2076-6-6')    
-		AND ec.companyId = @CoID -- Comment this out if you want full universe  
+                WHERE 1=1    
+                AND ec.TCSOFlag = 2 -- equity component should be included in total shares outstanding (as of today only, but we assume this flag usually does not change historically)    
+                AND @sharesDate BETWEEN ISNULL(sec.securityStartDate,'1/1/1900') 
+                AND ISNULL(sec.securityenddate,'2076-6-6')    
+                AND ec.companyId = @CoID -- Comment this out if you want full universe  
 ) so  
 
 GROUP BY so.companyId  
-ORDER BY so.companyId    
+ORDER BY so.companyId
