70dd67603747132e4cdada259791ea01b5011d25
Avinash1125 <63236641+Avinash1125@users.noreply.github.com>
2020-07-29 20:43:18 +0530

    Update ViewSourcesValuesAndFilingDates.sql

--- MSSQL/ViewSourcesValuesAndFilingDates.sql
+++ MSSQL/ViewSourcesValuesAndFilingDates.sql
@@ -1,14 +1,29 @@
-/***********************************************************************************************
-Returns The Sources, Values And Filing Dates
+/************************************************************************************************
+Displaying CIQ financials in reported and targeted Currency
 
 Packages Required:
-Finl Premium Core, Finl Premium Detail, Finl Premium Statement, Base Company, Base Data Item Master, Base Equity Security, Base Foundation Company Daily, Base Security
+Finl Premium Core
+Finl Premium Capital Structure
+Finl Premium Detail
+Finl Premium Market
+Finl Premium Ratio
+Finl Premium Segments
+Finl Premium Statement
+Finl Premium Supplemental
+Base Company 
+Base Data Item Master
+Base Exchange Rates
+Base Foundation Company Daily
 
 Universal Identifiers:
-companyId, tradingItemId, securityId
+companyId
 
 Primary Columns Used:
-Primary Columns used:, dataItemId, financialCollectionId, financialInstanceId, financialPeriodId, periodTypeId
+dataItemId
+financialCollectionId
+financialInstanceId
+financialPeriodId
+periodTypeId
 
 Database_Type:
 MSSQL
@@ -17,46 +32,54 @@ Query_Version:
 V1
 
 Query_Added_Date:
-25/05/2020
+03\06\2020
 
 DatasetKey:
 10
 
-The following sample SQL query displays the sources, values and filing dates for a specific company and data item as of a specific period end date
+This query returns a data item in the reported currency and translated into a target currency using the 
+SP Capital IQ Premium Financials packages and the v2 Exchange packages in Xpressfeed:
+NOTE:Displays item from SP Capital IQ Premium Financials package in reported currency and same item translated to target currency
 
 ***********************************************************************************************/
 
+SELECT c.companyName, 
+c.companyId, 
+co.country, 
+cu.currencyName, 
+fi.formtype, 
+fi.restatementTypeId, 
+fi.periodEndDate, 
+pt.periodTypeName, 
+fp.calendarQuarter, 
+fp.calendarYear, 
+fi.filingDate, 
+fd.dataItemId, 
+di.dataItemName, 
+fd.dataItemValue, 
+( fd.dataItemValue * cex2.priceClose ) / cex.priceClose AS DataItemValue_GBP
 
-SELECT c.companyName
-,c.companyId
-,ti.tickerSymbol
-,e.exchangeSymbol
-,co.country
-,fi.filingDate,
-fi.formType
-,fi.periodEndDate
-,pt.periodTypeName
-,fp.calendarQuarter
-,fp.calendarYear
-,fd.dataItemId
-,di.dataItemName
-,fd.dataItemValue
+FROM ciqCompany c
 
-FROM ciqCompany c JOIN ciqSecurity s ON c.companyId = s.companyId
-JOIN ciqTradingItem ti ON ti.securityId = s.securityId
-JOIN ciqExchange e ON e.exchangeId = ti.exchangeId
 JOIN ciqCountryGeo co ON co.countryId = c.countryId
 JOIN ciqFinPeriod fp ON fp.companyId = c.companyId
 JOIN ciqPeriodType pt ON pt.periodTypeId = fp.periodTypeId
 JOIN ciqFinInstance fi ON fi.financialPeriodId = fp.financialPeriodId
-JOIN ciqFinInstanceToCollection ic ON ic.financialInstanceId = fi.financialInstanceId 
+JOIN ciqFinInstanceToCollection ic ON ic.financialInstanceId = fi.financialInstanceId
 JOIN ciqFinCollection fc ON fc.financialCollectionId = ic.financialCollectionId
 JOIN ciqFinCollectionData fd ON fd.financialCollectionId = fc.financialCollectionId
+JOIN ciqCurrency cu ON cu.currencyid = fc.currencyId
 JOIN ciqDataItem di ON di.dataItemId = fd.dataItemId
-WHERE 1=1 
-AND fd.dataItemId = 15 --Net Income (IS)
+JOIN ciqExchangeRate cex ON cex.currencyId = fc.currencyId
+AND cex.priceDate = fi.periodEndDate
+JOIN ciqExchangeRate cex2 ON cex2.priceDate = cex.priceDate
+JOIN ciqCurrency cu2 ON cu2.currencyid = cex2.currencyid
+
+WHERE fd .dataItemId = 300 --Sales
+
 AND fp.periodTypeId = 1 --Annual
-AND e.exchangeSymbol = 'TSE' 
-AND ti.tickerSymbol = '7267' --Honda Motor Company
-AND fi.periodEndDate = '3/31/2007'  
-ORDER BY fi.filingDate
+AND c.Companyid=323452 -- Novo Nordisk A/S
+AND fi.periodEndDate = '12/31/2007'
+AND cu2.currencyName LIKE 'British Pound%' --Target Currency
+
+ORDER BY filingDate DESC
