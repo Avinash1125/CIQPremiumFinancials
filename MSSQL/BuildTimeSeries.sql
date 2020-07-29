dc68c695aaad98ffd290956a54c970aa162497fe
Avinash1125 <63236641+Avinash1125@users.noreply.github.com>
2020-07-29 23:26:57 +0530

    updated

--- MSSQL/BuildTimeSeries.sql
+++ MSSQL/BuildTimeSeries.sql
@@ -2,7 +2,14 @@
 Returns A Time Series
 
 Packages Required:
-Finl Premium Core, Finl Premium Detail, Finl Premium Statement, Base Company, Base Data Item Master, Base Equity Security, Base Foundation Company Daily, Base Security
+FinlPremiumCore
+FinlPremiumDetail
+FinlPremiumStatement
+BaseCompany
+BaseDataItemMaster
+BaseEquitySecurity
+BaseFoundationCompanyDaily
+BaseSecurity
 
 Universal Identifiers:
 companyId, tradingItemId, securityId
