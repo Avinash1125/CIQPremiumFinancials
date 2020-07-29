45656cc7937011f488689ea731e51a539b7ccf77
Avinash1125 <63236641+Avinash1125@users.noreply.github.com>
2020-07-29 23:07:21 +0530

    updated

--- MSSQL/CIQPremiumFinancialsPIVOT.sql
+++ MSSQL/CIQPremiumFinancialsPIVOT.sql
@@ -1,21 +1,14 @@
-/***********************************************************************
-CIQ Premium Financials PIVOT
+/***********************************************************************************************
+Ciq Premium Fundamentals Pivot
 
 Packages Required:
-Finl Premium Core
-Finl Premium Detail
-Base Company
-Base Data Item Master
-Base Foundation Company Daily
+Finl Premium Core, Finl Premium Detail, Base Company, Base Data Item Master, Base Foundation Company Daily
 
 Universal Identifiers:
 companyId
 
 Primary Columns Used:
-dataItemId
-financialCollectionId
-financialInstanceId
-financialPeriodId
+dataItemId, financialCollectionId, financialInstanceId, financialPeriodId
 
 Database_Type:
 MSSQL
@@ -24,18 +17,19 @@ Query_Version:
 V1
 
 Query_Added_Date:
-25\05\2020
+25/05/2020
 
 DatasetKey:
 10
 
-PIVOT's Premium Financials for a few specific items for a set CompanyID 
-for a set Calendar Year. LatestPeriodFinancialFlag=1
- 
-***********************************************************************/ 
+PIVOT\'s Premium Financials for a few specific items for a set CompanyID ", for a set Calendar Year. LatestPeriodFinancialFlag=1
+
+***********************************************************************************************/
+
+
 
 /**Drop Temporary Table if it already Exists**/  
-IF OBJECT_ID ('tempdb..#TempCIQPremium') is NOT NULL  
+IF OBJECT_ID (\'tempdb..#TempCIQPremium\') is NOT NULL  
 BEGIN  DROP TABLE #TempCIQPremium  
 END 
 GO    
@@ -46,42 +40,42 @@ GO
 
 /**Set InstitutionID and PeriodTypeID**/ 
  
-SET @companyid = '112350' ---IBM  
-SET @periodtypeId = '1' ---Annual=1, Quarterly=2, YTD=3, LTM=4, Semi-Annual=10, Interim=17  
-SET @calendarYear = '2014'  
-SET @latestforFinancialPeriodFlag = '1' --- Only Latest = 1    
+SET @companyid = \'112350\' ---IBM  
+SET @periodtypeId = \'1\' ---Annual=1, Quarterly=2, YTD=3, LTM=4, Semi-Annual=10, Interim=17  
+SET @calendarYear = \'2014\'  
+SET @latestforFinancialPeriodFlag = \'1\' --- Only Latest = 1    
 
 /**Put Stock Period Data and Flow Period Data into a Temp Table using a Union**/  
 
 SELECT * INTO #TempCIQPremium 
 FROM (    
 /**Bring in CIQ Premium Financials Data**/  
-	SELECT a.companyId   
-	, b.filingDate   
-	, b.latestForFinancialPeriodFlag      
-	, b.formType      
-	, g.dataItemName     
-	, e.dataItemValue       
-	, a.calendarYear          
-	FROM ciqFinPeriod a ( NOLOCK )      
-	JOIN ciqFinInstance b ( NOLOCK ) ON a.financialPeriodId = b.financialPeriodId      
-	JOIN ciqFinInstanceToCollection c ( NOLOCK ) ON b.financialInstanceId = c.financialInstanceId      
-	JOIN ciqFinCollection d ( NOLOCK ) ON c.financialCollectionId = d.financialCollectionId      
-	JOIN ciqFinCollectionData e ( NOLOCK ) ON d.financialCollectionId = e.financialCollectionId      
-	JOIN ciqCompany f ( NOLOCK ) ON a.companyId = f.companyId      
-	JOIN ciqDataItem g ( NOLOCK ) ON e.dataItemId = g.dataItemId   
-	WHERE 1=1      
-	AND a.calendarYear= @calendarYear      
-	AND a.periodTypeId = @periodtypeID      
-	AND a.companyId = @companyid      
-	AND b.latestForFinancialPeriodFlag = @latestforFinancialPeriodFlag       
+        SELECT a.companyId   
+        , b.filingDate   
+        , b.latestForFinancialPeriodFlag      
+        , b.formType      
+        , g.dataItemName     
+        , e.dataItemValue       
+        , a.calendarYear          
+        FROM ciqFinPeriod a ( NOLOCK )      
+        JOIN ciqFinInstance b ( NOLOCK ) ON a.financialPeriodId = b.financialPeriodId      
+        JOIN ciqFinInstanceToCollection c ( NOLOCK ) ON b.financialInstanceId = c.financialInstanceId      
+        JOIN ciqFinCollection d ( NOLOCK ) ON c.financialCollectionId = d.financialCollectionId      
+        JOIN ciqFinCollectionData e ( NOLOCK ) ON d.financialCollectionId = e.financialCollectionId      
+        JOIN ciqCompany f ( NOLOCK ) ON a.companyId = f.companyId      
+        JOIN ciqDataItem g ( NOLOCK ) ON e.dataItemId = g.dataItemId   
+        WHERE 1=1      
+        AND a.calendarYear= @calendarYear      
+        AND a.periodTypeId = @periodtypeID      
+        AND a.companyId = @companyid      
+        AND b.latestForFinancialPeriodFlag = @latestforFinancialPeriodFlag       
 ) CIQPremFin    
 
 /**Pivot Listed Data Item Names into Columns**/  
 SELECT * FROM #TempCIQPremium   
 PIVOT (MAX(dataitemvalue) for dataitemname in (      
-	 [Sales]      
-	,[Total Assets]      
-	,[Total Debt]      
-	,[Cost Of Goods Sold]      
-	,[Shares Outstanding on Filing Date])) AS dataitemvalues ---Insert Data Item Names to Bring In        
+         [Sales]      
+        ,[Total Assets]      
+        ,[Total Debt]      
+        ,[Cost Of Goods Sold]      
+        ,[Shares Outstanding on Filing Date])) AS dataitemvalues ---Insert Data Item Names to Bring In
